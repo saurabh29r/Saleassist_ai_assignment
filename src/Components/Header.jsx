9aa86dd46ada5a66a09b89a090c159e35eea2460
Saurabh <saurabh100rab002@gmail.com>
2024-02-16 15:01:17 +0530

    added the header section details

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,23 @@
 import "./Header.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Header() {
   return (
-    <div>
-      <h1 className="headers"> This is header</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div>
+            <ul className="list_style">
+              <li className="list-styles">Bags</li>
+              <li className="list-styles"> Cloths</li>
+              <li className="list-styles"> Travel</li>
+              <li className="list-styles"> Gifting</li>
+              <li className="list-styles"> Jewelery</li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
