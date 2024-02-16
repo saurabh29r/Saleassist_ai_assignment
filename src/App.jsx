e714a44d6eb50b3607ec518aefc44529f0210695
Saurabh <saurabh100rab002@gmail.com>
2024-02-16 14:39:16 +0530

    added the nav section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbars from "./Components/Navbars";
+import Header from "./Components/Header";
 function App() {
   return (
     <>
       <Navbars />
+      <Header />
     </>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="headers"> This is header</h1>
+    </div>
+  );
+}
+
+export default Header;
