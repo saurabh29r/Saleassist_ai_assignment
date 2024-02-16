033c8d71e3fa471ada19f12d84219d5d0e71ab71
Saurabh <saurabh100rab002@gmail.com>
2024-02-16 21:18:18 +0530

    added the filter prducts

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -11,14 +11,14 @@ function Productcard() {
   const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  const url = "https://dummyjson.com/products";
+  const url = "https://myfirstapi-data.vercel.app";
 
   const getProduct = async () => {
     try {
       const res = await axios.get(url);
-
-      setProducts(res.data.products);
       console.log(res.data);
+      setProducts(res.data.products);
+
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -34,17 +34,17 @@ function Productcard() {
   );
 
   return (
-    <Container>
+    <Container fluid>
       <Row>
         <Col>
           <div>
-            <h2 className="mains-heading"> All Products</h2>
+            <h2 className="mains-heading ml-2"> All Products</h2>
           </div>
           <div className="input-wrapper">
             <FaSearch id="search-icon" />
             <input
               type="search"
-              placeholder="Please Enter the product Like:watch,toy etc"
+              placeholder="Please Enter the product Like:T-shirt,watch etc"
               onChange={(e) => setSearch(e.target.value)}
             />
           </div>
@@ -56,34 +56,24 @@ function Productcard() {
             ) : filteredProducts.length === 0 ? (
               <div className="empty-search-conatiner">
                 <div className="empty-search">
-                  <img
-                    src={
-                      "https://assets.ccbp.in/frontend/react-js/nxt-trendz/nxt-trendz-no-products-view.png"
-                    }
-                    alt="No results found"
-                    className="no-product-found"
-                  />
                   <h3> No Products Found</h3>
                   <p> Search something else</p>
                 </div>
               </div>
             ) : (
               filteredProducts.map((item, index) => {
-                const { brand, image_url, price, rating, title } = item;
+                const { image, price, title } = item;
 
                 return (
                   <LazyLoad key={index}>
                     <div className="product-container">
-                      <img src={image_url} alt="" className="map-image" />
+                      <img src={image} alt="" className="map-image" />
                       <div className="card-body">
                         <h5 className="card-title text-center pt-2">
-                          {title.substr(0, 30)}
+                          {title.substr(0, 20)}
                         </h5>
-                        <p className="card-text text-center">{`by ${brand}`}</p>
-                        <p className="text-center price">{`₹ ${price}`}</p>
-                        <div className="rating-container">
-                          <p className="ratings text-center">{`${rating} ⭐`}</p>
-                        </div>
+
+                        <p className="text-center price pt-2">{`₹ ${price}`}</p>
                         <div className="btns text-center">
                           <button className="btns">Add to Cart</button>
                         </div>
